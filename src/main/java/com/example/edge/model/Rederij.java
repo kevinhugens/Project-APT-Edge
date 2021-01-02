d71058f7ea01bd1b0b242af53cfb22c3fee7fcc1
kevin <kevin@KEVIN-MBP>
2021-01-02 17:19:22 +0100

    Added docker and deployfile

--- src/main/java/com/example/edge/model/Rederij.java
+++ src/main/java/com/example/edge/model/Rederij.java
@@ -1,14 +1,6 @@
 package com.example.edge.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 public class Rederij {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int rederijID;
 
     private String naam;
